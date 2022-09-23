e2d02d2628a96b1d6c43debc5508da02baa8367f
VVPCodes <pomyateevav@gmail.com>
2022-09-22 20:30:53 -0400

    made changes to the page

--- index.js
+++ index.js
@@ -1,2 +1,24 @@
 
 // Select some elements...
+let header = document.querySelector('#page-header');
+header.style.textAlign = 'left';
+
+//function for the loops 
+let dogImages = document.querySelectorAll('.dog-image');
+for (let i = 0; i < dogImages.length; i++) {
+    dogImages[i].style.borderRadius = '50px'
+}
+for (let i = 0; i < dogImages.length; i++) {
+    dogImages[i].style.transform = 'scaleY(-1)'
+}
+
+let dogNames = document.querySelectorAll('.dog-name');
+for (let i = 0; i < dogNames.length; i++) {
+    dogNames[i].style.textAlign = 'left'
+}
+
+let footer = document.querySelector('.footer')
+footer.style.color = 'purple';
+// why this does not work? 
+footer.style.borderStyle = 'red';
+
